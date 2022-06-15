f62b6c1b3e60720fdc6d6915282a85c7dac59881
Alex Birch <Birch-san@users.noreply.github.com>
2022-06-16 00:34:28 +0100

    repro which doesn't require model

--- repro_minimal_2.py
+++ repro_minimal_2.py
@@ -3,7 +3,6 @@ from numpy import array_equal, vectorize, uint8
 import torch as th
 from torch import tensor
 import cv2
-from basicsr.archs.rrdbnet_arch import RRDBNet
 
 def choose_colour (row_ix: uint8, col_ix: uint8, _channel_ix: uint8) -> uint8:
   if row_ix == 1 and col_ix == 1:
@@ -12,24 +11,8 @@ def choose_colour (row_ix: uint8, col_ix: uint8, _channel_ix: uint8) -> uint8:
     return 255
   return 0
 
-def make_img(device_type, half):
-  scale = 4
-  tile_size = 0
-  tile_pad = 10
-  pre_pad = 0
-  mod_scale = None
-  half = False
-  model_path = './Real-ESRGAN/experiments/pretrained_models/RealESRGAN_x4plus.pth'
-  loadnet = th.load(model_path, map_location=th.device('cpu'))
-  realesrgan_model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64,
-                           num_block=23, num_grow_ch=32, scale=4)
-  realesrgan_model.load_state_dict(loadnet['params_ema'], strict=True)
-  realesrgan_model.eval()
-  realesrgan_model = realesrgan_model.to(device_type)
-  if half:
-    realesrgan_model = realesrgan_model.half()
+def make_img(device_type):
   img = np.fromfunction(function=vectorize(choose_colour), shape=(3, 3, 3), dtype=uint8)
-  h_input, w_input = img.shape[0:2]
   img = img.astype(np.float32)
   img = img / 255
   img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -37,9 +20,28 @@ def make_img(device_type, half):
   img = img.unsqueeze(0).to(device_type)
   return img
 
+def make_img_unit(device_type):
+  input = tensor(
+  [[[0.0000, 0.0000, 0.0000],
+    [0.0000, 0.4980, 1.0000],
+    [0.0000, 1.0000, 1.0000]],
+
+  [[0.0000, 0.0000, 0.0000],
+    [0.0000, 0.4980, 1.0000],
+    [0.0000, 1.0000, 1.0000]],
+
+  [[0.0000, 0.0000, 0.0000],
+    [0.0000, 0.4980, 1.0000],
+    [0.0000, 1.0000, 1.0000]]])
+  return input.unsqueeze(0).to(device_type)
+
 half = False
-cpu_output = make_img('cpu', half)
-mps_output = make_img('mps', half)
+cpu_output = make_img('cpu')
+mps_output = make_img('mps').cpu()
+# cpu_output = make_img_unit('cpu')
+# mps_output = make_img_unit('mps').cpu()
+print('cpu_output:\n', cpu_output)
+print('mps_output:\n', mps_output)
 assert array_equal(cpu_output, mps_output), "unsqueezed MPS tensor differs from CPU counterpart"
 
 # cpu_output = input.unsqueeze(0).to('cpu')
