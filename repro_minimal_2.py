262732b8ca628b0e89dfcf8d8dc2bb88317eff9f
Alex Birch <Birch-san@users.noreply.github.com>
2022-06-16 21:06:40 +0100

    simpler

--- repro_minimal_2.py
+++ repro_minimal_2.py
@@ -4,46 +4,46 @@ import torch as th
 from torch import tensor, allclose
 
 arr = array([[[0.        , 0.        , 0.        ],
-        [0.        , 0.        , 0.        ],
-        [0.        , 0.        , 0.        ]],
+        [0.        , 0.49803922, 1.        ],
+        [0.        , 1.        , 1.        ]],
 
        [[0.        , 0.        , 0.        ],
-        [0.49803922, 0.49803922, 0.49803922],
-        [1.        , 1.        , 1.        ]],
+        [0.        , 0.49803922, 1.        ],
+        [0.        , 1.        , 1.        ]],
 
        [[0.        , 0.        , 0.        ],
-        [1.        , 1.        , 1.        ],
-        [1.        , 1.        , 1.        ]]], dtype=float32)
+        [0.        , 0.49803922, 1.        ],
+        [0.        , 1.        , 1.        ]]], dtype=float32)
 
 def get_tensor():
   arr_t = np.transpose(arr, (2, 0, 1))
-  img = th.from_numpy(arr_t).float()
+  img = th.from_numpy(arr_t)
   img = img.unsqueeze(0)
   return img
 
-hardcoded_tensor = tensor([[[[0.0000, 0.0000, 0.0000],
-  [0.0000, 0.4980, 1.0000],
-  [0.0000, 1.0000, 1.0000]],
+hardcoded_tensor = tensor([[[0.0000, 0.0000, 0.0000],
+         [0.0000, 0.0000, 0.0000],
+         [0.0000, 0.0000, 0.0000]],
 
-  [[0.0000, 0.0000, 0.0000],
-  [0.0000, 0.4980, 1.0000],
-  [0.0000, 1.0000, 1.0000]],
+        [[0.0000, 0.4980, 1.0000],
+         [0.0000, 0.4980, 1.0000],
+         [0.0000, 0.4980, 1.0000]],
 
-  [[0.0000, 0.0000, 0.0000],
-  [0.0000, 0.4980, 1.0000],
-  [0.0000, 1.0000, 1.0000]]]])
+        [[0.0000, 1.0000, 1.0000],
+         [0.0000, 1.0000, 1.0000],
+         [0.0000, 1.0000, 1.0000]]])
 
-unsqueezed_via_cpu = hardcoded_tensor.to('cpu')
-unsqueezed_via_mps = hardcoded_tensor.to('mps').cpu()
-assert allclose(unsqueezed_via_cpu, unsqueezed_via_mps, rtol=0.0001), "expect {hardcoded_tensor -> CPU} == {hardcoded_tensor -> GPU -> CPU}"
+via_cpu = hardcoded_tensor.to('cpu')
+via_mps = hardcoded_tensor.to('mps').cpu()
+assert allclose(via_cpu, via_mps, rtol=0.0001), "expect {hardcoded_tensor -> CPU} == {hardcoded_tensor -> GPU -> CPU}"
 
 computed_tensor = get_tensor()
 print('hardcoded_tensor:\n', hardcoded_tensor)
 print('computed_tensor:\n', computed_tensor)
 assert allclose(computed_tensor, hardcoded_tensor, rtol=0.0001), "expect hardcoded_tensor == get_tensor()"
 
-unsqueezed_via_cpu = computed_tensor.to('cpu')
-unsqueezed_via_mps = computed_tensor.to('mps').cpu()
-print('unsqueezed_via_cpu:\n', unsqueezed_via_cpu)
-print('unsqueezed_via_mps:\n', unsqueezed_via_mps)
-assert allclose(unsqueezed_via_cpu, unsqueezed_via_mps, rtol=0.0001), "expect {get_tensor() -> CPU} == {get_tensor() -> GPU -> CPU}" # fails!
+via_cpu = computed_tensor.to('cpu')
+via_mps = computed_tensor.to('mps').cpu()
+print('via_cpu:\n', via_cpu)
+print('via_mps:\n', via_mps)
+assert allclose(via_cpu, via_mps, rtol=0.0001), "expect {get_tensor() -> CPU} == {get_tensor() -> GPU -> CPU}" # fails!
