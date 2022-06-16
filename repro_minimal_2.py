fef04d281336321d55656a8892444a7ba3ea25fb
Alex Birch <Birch-san@users.noreply.github.com>
2022-06-16 21:33:00 +0100

    simpler

--- repro_minimal_2.py
+++ repro_minimal_2.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy import float32, array
 import torch as th
-from torch import tensor, allclose
+from torch import tensor
 
 arr = array([[[0, 0],
         [0, 1]],
@@ -22,14 +22,14 @@ hardcoded_tensor = tensor([[[[0., 0.],
           [0., 1.]]]])
 
 via_mps = hardcoded_tensor.to('mps').cpu()
-assert allclose(hardcoded_tensor, via_mps, rtol=0.0001), "hardcoded_tensor == {hardcoded_tensor -> GPU -> CPU}"
+assert th.equal(hardcoded_tensor, via_mps), "hardcoded_tensor == {hardcoded_tensor -> GPU -> CPU}"
 
 computed_tensor = get_tensor()
 print('hardcoded_tensor:\n', hardcoded_tensor)
 print('computed_tensor:\n', computed_tensor)
-assert allclose(computed_tensor, hardcoded_tensor, rtol=0.0001), "hardcoded_tensor == get_tensor()"
+assert th.equal(computed_tensor, hardcoded_tensor), "hardcoded_tensor == get_tensor()"
 
 via_mps = computed_tensor.to('mps').cpu()
 print('computed_on_cpu:\n', computed_tensor)
 print('computed_via_mps:\n', via_mps)
-assert allclose(computed_tensor, via_mps, rtol=0.0001), "get_tensor() == {get_tensor() -> GPU -> CPU}" # fails!
+assert th.equal(computed_tensor, via_mps), "get_tensor() == {get_tensor() -> GPU -> CPU}" # fails!
